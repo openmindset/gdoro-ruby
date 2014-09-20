2291c64378a74a958cccdd6bc9f71149e3b36885
openmindset <fdavis712@gmail.com>
2014-09-20 00:31:40 -0400

    timer correctly resets to break/post-break values

--- gdoro-ruby.rb
+++ gdoro-ruby.rb
@@ -11,8 +11,12 @@ Shoes.app :width => 260,
   background "#EFC"
   border("#BE8", :strokewidth => 6)
 
-  # initialially set timer to 25:00
+  # Seconds is our timer variable. It will decrement to 0.
   @seconds = 1500
+  # We need a placeholder to detect what the last timer cycle value was.
+  # This will initially be set to 1500 (25:00) as all work begins with that
+  # cycle as a default.
+  @seconds_previous = 1500
   @paused = true
   @pomo_count = 0
 
@@ -42,7 +46,7 @@ Shoes.app :width => 260,
   end
 
   @timer = stack :width => 320, :height => 50 do
-    title("25:00",
+    title("#{display_time}",
           align: "center")
   end
 
@@ -69,18 +73,34 @@ Shoes.app :width => 260,
           :margin_top => 16
   end
 
-  animate(1) do
-    if @seconds > 1
-      @seconds -= 1 unless @paused
-      display_time
-    else
-      # detect if the last pomo completed was a 25 cycle or a 5 break
-      # also, when pomo complete count is 4, notify to take break
-      @pomo_count += 1
-      @paused = !@paused
-      @seconds = 300
-      update_pomo_count
-      # need to code return to 25:00 minutes after 5:00 break
-    end
+  if @paused # don't loop the block if paused is true, otherwise it's buggy
+    animate(1) do
+      if @seconds > 1
+        @seconds -= 1 unless @paused
+        display_time
+      else
+        # detect if the last pomo completed was a 25 cycle or a 5 break
+        # also, when pomo complete count is 4, notify to take break
+        case @seconds_previous
+        when 1500
+          alert("Pomodoro complete!")
+          @pomo_count += 1
+          @paused = !@paused
+          @seconds = 300
+          @seconds_previous = 300
+          @btn_start.real.set_label("Start")
+          update_pomo_count
+        when 300
+          alert("Break's over!")
+          @paused = !@paused
+          @seconds = 1500
+          @seconds_previous = 1500
+          puts @seconds_previous
+          @btn_start.real.set_label("Start")
+        else
+          alert("The timer broke!")  
+        end       
+      end
+    end  
   end
 end
